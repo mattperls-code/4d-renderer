206285301e4f2e50c5a7d68cffb36fbdb954323f
mattperls-code <mattperls.code@gmail.com>
2022-05-19 09:49:56 -0400

    new demo, more visual but less mathematically accurate

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ class Vec3 {
         this.z = z
     }
 
+    static subtract(v1, v2){
+        return new Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
+    }
+
     static scalarMultiply(v, s){
         return new Vec3(v.x * s, v.y * s, v.z * s)
     }
@@ -33,17 +37,14 @@ class Vec4 {
         this.z = z
     }
 
-    static projectionBase = new Vec3(1, 1, 1)
+    static projectionScale = 0
+    static projectionOffset = new Vec3(0, 0, 0)
 
-    static subtract(v1, v2){
-        return new Vec4(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w - v2.w)
-    }
-
-    to3D(){
+    static to3D(v, cameraW){
         return new Vec3(
-            this.x + this.w * Vec4.projectionBase.x,
-            this.y + this.w * Vec4.projectionBase.y,
-            this.z + this.w * Vec4.projectionBase.z
+            v.x - (v.x * (v.w - cameraW) * Vec4.projectionScale) + (Vec4.projectionOffset.x * (v.w - cameraW)),
+            v.y - (v.y * (v.w - cameraW) * Vec4.projectionScale) + (Vec4.projectionOffset.y * (v.w - cameraW)),
+            v.z - (v.z * (v.w - cameraW) * Vec4.projectionScale) + (Vec4.projectionOffset.y * (v.w - cameraW))
         )
     }
 }
@@ -58,12 +59,14 @@ class Camera {
 }
 
 class Scene {
-    constructor(camera, segments, vertexColor, edgeColor, bgColor){
+    constructor(camera, segments, vertexColor, dimensionalEdgeColor, firstDimensionColor, secondDimensionColor, bgColor){
         this.camera = camera
         this.segments = segments
 
         this.vertexColor = vertexColor
-        this.edgeColor = edgeColor
+        this.dimensionalEdgeColor = dimensionalEdgeColor
+        this.firstDimensionColor = firstDimensionColor
+        this.secondDimensionColor = secondDimensionColor
         this.bgColor = bgColor
     }
 
@@ -79,11 +82,11 @@ class Scene {
         const screenSpaceSegments = []
         
         this.segments.forEach(segment => {
-            let p1 = Vec4.subtract(segment[0], this.camera.pos)
-            let p2 = Vec4.subtract(segment[1], this.camera.pos)
+            let p1 = Vec4.to3D(segment[0], this.camera.pos.w)
+            let p2 = Vec4.to3D(segment[1], this.camera.pos.w)
 
-            p1 = p1.to3D()
-            p2 = p2.to3D()
+            p1 = Vec3.subtract(p1, this.camera.pos)
+            p2 = Vec3.subtract(p2, this.camera.pos)
 
             p1 = new Vec3(p1.x * cosRotY + p1.z * sinRotY, p1.y, p1.z * cosRotY - p1.x * sinRotY)
             p2 = new Vec3(p2.x * cosRotY + p2.z * sinRotY, p2.y, p2.z * cosRotY - p2.x * sinRotY)
@@ -92,22 +95,41 @@ class Scene {
             p2 = new Vec3(p2.x, p2.y * cosRotX - p2.z * sinRotX, p2.y * sinRotX + p2.z * cosRotX)
 
             if(p1.z > this.camera.near && p2.z > this.camera.near){
-                p1 = new Vec2(p1.x / p1.z / Math.tan(this.camera.fov / 2), p1.y / p1.z / Math.tan(this.camera.fov / 2))
-                p2 = new Vec2(p2.x / p2.z / Math.tan(this.camera.fov / 2), p2.y / p2.z / Math.tan(this.camera.fov / 2))
+                p1 = new Vec3(p1.x / p1.z / Math.tan(this.camera.fov / 2), p1.y / p1.z / Math.tan(this.camera.fov / 2), p1.z)
+                p2 = new Vec3(p2.x / p2.z / Math.tan(this.camera.fov / 2), p2.y / p2.z / Math.tan(this.camera.fov / 2), p2.z)
 
-                screenSpaceSegments.push([ p1, p2 ])
+                let color = this.dimensionalEdgeColor
+
+                if(segment[0].w == segment[1].w){
+                    if(segment[0].w == 0){
+                        color = this.firstDimensionColor
+                    } else {
+                        color = this.secondDimensionColor
+                    }
+                }
+
+                screenSpaceSegments.push([ p1, p2, color ])
             }
         })
-        
+
+        screenSpaceSegments.sort((s1, s2) => {
+            if(s1[0].z < s2[0].z && s1[1].z < s2[1].z){
+                return 1
+            } else {
+                return -1
+            }
+        })
+
         ctx.lineWidth = 4
-        ctx.strokeStyle = this.edgeColor
-        ctx.beginPath()
+
         screenSpaceSegments.forEach(segment => {
+            ctx.strokeStyle = segment[2]
+            ctx.beginPath()
             ctx.moveTo(0.5 * canvas.width + segment[0].x * canvas.width, 0.5 * canvas.height - segment[0].y * canvas.width)
             ctx.lineTo(0.5 * canvas.width + segment[1].x * canvas.width, 0.5 * canvas.height - segment[1].y * canvas.width)
+            ctx.closePath()
+            ctx.stroke()
         })
-        ctx.closePath()
-        ctx.stroke()
 
         ctx.fillStyle = this.vertexColor
         screenSpaceSegments.forEach(segment => {
@@ -130,8 +152,8 @@ const ctx = canvas.getContext("2d")
 
 const mainScene = new Scene(
     new Camera(
-        new Vec4(0, 2.5, -7.5, 0),
-        new Vec2(0, 0),
+        new Vec4(0, 2.5, -10, 0.5),
+        new Vec2(-10 * Math.PI / 180, 0),
         90 * Math.PI / 180,
         1
     ),
@@ -274,8 +296,10 @@ const mainScene = new Scene(
             new Vec4(-1, -1, 1, 1)
         ]
     ],
-    "rgb(100, 50, 200)",
-    "rgb(150, 100, 250)",
+    "rgb(100, 200, 100)",
+    "rgb(0, 255, 0)",
+    "rgb(250, 0, 0)",
+    "rgb(0, 0, 250)",
     "rgb(25, 0, 50)"
 )
 
@@ -288,6 +312,8 @@ window.addEventListener("keyup", (event) => {
     keyMap[event.key] = false
 })
 
+let t = 0
+let last = performance.now()
 const updateScene = () => {
     if(keyMap["ArrowLeft"]){
         mainScene.camera.pos.x += 0.1 * Math.cos(mainScene.camera.rot.y + Math.PI)
@@ -322,6 +348,14 @@ const updateScene = () => {
     mainScene.camera.pos.w = parseFloat(document.getElementById("w").value)
     mainScene.camera.rot.x = parseFloat(document.getElementById("rotX").value)
     mainScene.camera.fov = parseFloat(document.getElementById("fov").value * Math.PI / 180)
+
+    const dt = performance.now() - last
+    t += 0.001 * dt
+    last = performance.now()
+
+    Vec4.projectionScale = 0.5 * Math.sin(t)
+
+    Vec4.projectionOffset.x = 1.5 * Math.cos(t)
 
     mainScene.render(canvas, ctx)
     requestAnimationFrame(updateScene)
